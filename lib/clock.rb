93f306353a92d8ee7b557c188db9e838d4e014dd
superkevin@gmail.com <superkevin@gmail.com>
2017-01-15 02:36:46 -0500

    simplify the clock task for now

--- lib/clock.rb
+++ lib/clock.rb
@@ -3,6 +3,6 @@ require File.expand_path('../../config/boot',        __FILE__)
 require File.expand_path('../../config/environment', __FILE__)
 include Clockwork
 
-every 2.seconds, 'update_departures_channel' do
-  ActionCable.server.broadcast '', DeparturesCollection.all
+every 15.seconds, 'update_stop_times' do
+  StopTimesUpdate.new.update_stop_times!
 end
